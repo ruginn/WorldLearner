2b194d5c176a6f3343524318f172964b927a8258
Rcatarata <Rcatarata@gmail.com>
2025-08-16 22:52:45 -0400

    map

---
+++ worlder/app/map/page.tsx
@@ -0,0 +1,51 @@
+'use client';
+import World from '@react-map/world';
+import { useEffect, useState } from 'react';
+import stateCode from './helper/states';
+
+export default function Map() {
+  if (typeof window === 'undefined') {
+    return <div>hello</div>;
+  }
+
+  useEffect(() => {
+    console.log(stateCode[Math.floor(Math.random() * stateCode.length)]);
+  });
+
+  const printer = (
+    state: string | null,
+    selectedStates?: string[] | undefined
+  ) => {
+    console.log(state);
+  };
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const hinter = true;
+
+  return (
+    <div className='flex flex-col justify-center items-center w-full bg-blue-400'>
+      <World
+        onSelect={printer}
+        size={windowWidth / 1.5}
+        // hints={hinter}
+        mapColor='#33b864'
+        hoverColor='red'
+        type='select-single'
+      />
+    </div>
+  );
+}
