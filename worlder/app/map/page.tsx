2347188b1ba2239f9768cf928957981f6ebb4f79
Rcatarata <Rcatarata@gmail.com>
2025-08-17 10:10:33 -0400

    Update page.tsx

--- worlder/app/map/page.tsx
+++ worlder/app/map/page.tsx
@@ -6,43 +6,43 @@ import stateCode from './helper/states';
 export default function Map() {
   if (typeof window === 'undefined') {
     return <div>hello</div>;
+  } else {
+    useEffect(() => {
+      console.log(stateCode[Math.floor(Math.random() * stateCode.length)]);
+    }, []);
+
+    const printer = (state: string | null) => {
+      console.log(state);
+    };
+
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      // Cleanup the event listener on component unmount
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
+    }, []);
+
+    //   const hinter = true;
+
+    return (
+      <div className='flex flex-col justify-center items-center w-full bg-blue-400'>
+        <World
+          onSelect={printer}
+          size={windowWidth / 1.5}
+          // hints={hinter}
+          mapColor='#33b864'
+          hoverColor='red'
+          type='select-single'
+        />
+      </div>
+    );
   }
-
-  useEffect(() => {
-    console.log(stateCode[Math.floor(Math.random() * stateCode.length)]);
-  }, []);
-
-  const printer = (state: string | null) => {
-    console.log(state);
-  };
-
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    // Cleanup the event listener on component unmount
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  //   const hinter = true;
-
-  return (
-    <div className='flex flex-col justify-center items-center w-full bg-blue-400'>
-      <World
-        onSelect={printer}
-        size={windowWidth / 1.5}
-        // hints={hinter}
-        mapColor='#33b864'
-        hoverColor='red'
-        type='select-single'
-      />
-    </div>
-  );
 }
